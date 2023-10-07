963c291dfcd895b9919d7256a3e7f19f49e9724c
Furkan Ozdeslik <furkan.ozdeslik@gmail.com>
2023-10-07 09:00:50 -0700

    add employee type id

--- events/user_created.go
+++ events/user_created.go
@@ -7,11 +7,12 @@ import (
 )
 
 type UserCreated struct {
-	UserId     uuid.UUID  `json:"userId"`
-	Email      string     `json:"email"`
-	IsEmployee bool       `json:"isEmployee"`
-	BirthDate  time.Time  `json:"birthDate"`
-	GivenName  string     `json:"givenName"`
-	FamilyName string     `json:"familyName"`
-	PlaceId    *uuid.UUID `json:"placeId"`
+	UserId         uuid.UUID  `json:"userId"`
+	Email          string     `json:"email"`
+	IsEmployee     bool       `json:"isEmployee"`
+	BirthDate      time.Time  `json:"birthDate"`
+	GivenName      string     `json:"givenName"`
+	FamilyName     string     `json:"familyName"`
+	PlaceId        *uuid.UUID `json:"placeId"`        // PlaceId is not needed for regular clients
+	EmployeeTypeId *int       `json:"employeeTypeId"` // EmployeeTypeId is not needed for regular clients
 }
---
+++ models/employee_type_id.go
@@ -0,0 +1,7 @@
+package models
+
+const (
+	Admin   = 1000
+	Manager = 2000
+	Staff   = 3000
+)
