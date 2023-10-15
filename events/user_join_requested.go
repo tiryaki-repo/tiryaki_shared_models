9fb3e3af57a478fe4d4ca469d42d5f5abf76d05a
Furkan Ozdeslik <furkan.ozdeslik@gmail.com>
2023-10-14 21:43:06 -0700

    update user join requested event

--- events/user_join_requested.go
+++ events/user_join_requested.go
@@ -5,9 +5,11 @@ import (
 )
 
 type UserJoinRequested struct {
-	RequesterUserId uuid.UUID `json:"requesterUserId" validate:"required"`
-	RequesteeUserId uuid.UUID `json:"requesteeUserId" validate:"required"`
-	PlaceId         uuid.UUID `json:"placeId" validate:"required"`
-	EmployeeTypeId  int       `json:"employeeTypeId" validate:"required"`
-	Code            uuid.UUID `json:"code" validate:"required"` // code is used for signupcode in auth
+	RequesterUserId    uuid.UUID  `json:"requesterUserId" validate:"required"`
+	Email              string     `json:"email" validate:"required"`
+	RequesteeUserId    *uuid.UUID `json:"requesteeUserId"`
+	RequesteeFirstName *string    `json:"RequesteeFirstName"`
+	PlaceId            uuid.UUID  `json:"placeId" validate:"required"`
+	EmployeeTypeId     int        `json:"employeeTypeId" validate:"required"`
+	Code               uuid.UUID  `json:"code" validate:"required"` // code is used for signupcode in auth
 }
